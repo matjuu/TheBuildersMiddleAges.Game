69b1710747ae9039d1ac179cdb304f476457b286
Matas Jurkšaitis <iammatjuu@gmail.com>
2016-03-04 13:33:31 +0200

    Added a dummy API Host project with placeholder controllers to make initial integration easier for client app.

---
+++ TheBuildesrMiddleAges.Game.Host/Controllers/GameController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+
+namespace TheBuildesrMiddleAges.Game.Host.Controllers
+{
+    public class GameController : Controller
+    {
+        [HttpPost]
+        [Route("api/game/start")]
+        public Guid StartGame()
+        {
+            var roomGuid = Guid.NewGuid();
+            return roomGuid;
+        }
+
+        [HttpPost]
+        [Route("api/game/state")]
+        public dynamic GetGameState()
+        {
+            return new
+            {
+                Board = "Dummy board object",
+                Players = new dynamic[]
+                {
+                    new
+                    {
+                        Guid = Guid.NewGuid()
+                    }
+                }     
+            };
+        }
+
+        [HttpPost]
+        [Route("api/worker/take")]
+        public string TakeWorker()
+        {
+            return "Worker has been successfully taken.";
+        }
+
+        [HttpPost]
+        [Route("api/building/take")]
+        public string TakeBuilding()
+        {
+            return "Building has been successfully taken";
+
+        }
+
+        [HttpPost]
+        [Route("api/worker/assign")]
+        public string AssignWorker()
+        {
+            return "Worker has been successfully assigned.";
+        }
+    }
+}
