7a37aea3d50c3eb3b320867ba1e92be76fa3443b
Matas Jurkšaitis <iammatjuu@gmail.com>
2016-03-09 01:46:32 +0200

    Added a basic constructor for the Game object in the case of a 1Player scenario

--- TheBuildersMiddleAges.Game.Core/Game.cs
+++ TheBuildersMiddleAges.Game.Core/Game.cs
@@ -5,10 +5,26 @@ namespace TheBuildersMiddleAges.Game.Core
 {
     public class Game
     {
-        private Dictionary<Guid, Player> _players;
-        private GameBoard _gameBoard;
+        private Dictionary<Guid, Player> _players = new Dictionary<Guid, Player>();
+        private GameBoard _gameBoard = new GameBoard();
         private Deck<Worker> _workersDeck;
         private Deck<Building> _buildingsDeck;
         private GameState _gameState;
+
+        Game(Guid playerGuid)
+        {
+            _players.Add(playerGuid, new Player());
+
+            _workersDeck.Shuffle();
+            _buildingsDeck.Shuffle();
+
+            PopulateGameboard();
+        }
+
+        private void PopulateGameboard()
+        {
+            //TODO: A method which draws cards and puts them on the Game Board
+            throw new NotImplementedException();
+        }
     }
 }
