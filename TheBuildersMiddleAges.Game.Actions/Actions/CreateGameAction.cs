d61e7d991a739f134182ed894370c8b90ffef3f2
RokasVaitkevicius <vaitkevicius.rokas@gmail.com>
2016-05-02 15:50:20 +0300

    Moved game creating logic from constructor to method do in CreateGameAction.

--- TheBuildersMiddleAges.Game.Actions/Actions/CreateGameAction.cs
+++ TheBuildersMiddleAges.Game.Actions/Actions/CreateGameAction.cs
@@ -6,23 +6,15 @@ using TheBuildersMiddleAges.Game.Infrastructure;
 namespace TheBuildersMiddleAges.Game.Actions.Actions
 {
     public class CreateGameAction: ActionBase<CreateGameActionResponse>
-    {
-        public List<Guid> Players;
-        public Guid NewGame;
-
-        public CreateGameAction()
-        {
-            Players = new List<Guid> {Guid.NewGuid()};
-            NewGame = GameContainer.Instance.CreateGame(Players);
-        }
-
+    { 
         public override CreateGameActionResponse Do(ActionRequest request)
         {
-
+            List<Guid> players = new List<Guid> {Guid.NewGuid()};
+            Guid newGame = GameContainer.Instance.CreateGame(players);
             CreateGameActionResponse response = new CreateGameActionResponse
             {
-                gameGuid = NewGame,
-                playerGuid = Players.First(),
+                gameGuid = newGame,
+                playerGuid = players.First(),
                 Success = true
             };
 
